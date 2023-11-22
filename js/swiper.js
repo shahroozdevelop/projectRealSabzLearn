44ec1e3b61d8e939e6b75871ae298ec8782eb2e8
shahrooz <shahroozdev@gmail.com>
2023-11-22 12:25:18 +0330

    add main project

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+    speed: 200,
+    loop: true,
+    breakpoints: {
+        576: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+
+})
